732413ee4c2ca0171d3ffeeb3c4df71309a5c77f
itidelCloud <46874618+itidelCloud@users.noreply.github.com>
2019-01-26 18:36:12 +0100

    Add files via upload

---
+++ final/src/main/java/tn/itidel/Controller/RequestController.java
@@ -0,0 +1,17 @@
+package tn.itidel.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class RequestController {
+
+	@RequestMapping(name="hello", method=RequestMethod.GET)
+	public String sayHello(@RequestParam String name){
+		return "Hello "+name;
+	}
+}
